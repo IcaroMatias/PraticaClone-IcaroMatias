d369eea01c0f2e7fedea63c0fed82d33e66276c6
Icaro <0001155469@senaimgaluno.com.br>
2025-11-03 11:00:02 -0300

    Atualização Print

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("EU SOU O CARA MAIS BONITO DO BRASIL");
+        System.out.printf("ICARO É O CARA MAIS BONITO DO BRASIL");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
